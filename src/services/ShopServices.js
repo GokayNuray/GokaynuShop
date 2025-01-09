7222d4d439b4f13f92571623ca4f56ee40d185a0
GokayNuray <gokay.nuray@ug.bilkent.edu.tr>
2025-01-09 09:28:01 +0300

    Changed getShopItems to utilize session storage

--- src/services/ShopServices.js
+++ src/services/ShopServices.js
@@ -9,10 +9,20 @@ export class ShopItem {
     }
 }
 
-export function getShopItems() {
+function fetchShopItems() {
+    console.log("Fetching items");
     let items = [];
     for (let i = 0; i < 17; i++) {
         items.push(new ShopItem(i, "Item " + i, 10 + i, "Description for item " + i, "https://www.w3schools.com/howto/img_avatar.png", new Date(Math.random() * 100000000000)));
+    }
+    sessionStorage.setItem("items", JSON.stringify(items));
+}
+
+export function getShopItems() {
+    let items = JSON.parse(sessionStorage.getItem("items"));
+    if (!items) {
+        fetchShopItems();
+        items = JSON.parse(sessionStorage.getItem("items"));
     }
     return items;
 }
